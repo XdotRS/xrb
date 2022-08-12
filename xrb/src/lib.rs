8e2bdf662c4071046319e0a9a1fef54f33d0751a
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-08-12 13:21:43 +0400

    XRB: tweak crate docs

--- xrb/src/lib.rs
+++ xrb/src/lib.rs
@@ -7,13 +7,14 @@
 //! [X11 protocol specification](https://x.org/releases/X11R7.7/doc/xproto/xprotocol.html/). XRB is
 //! _not_ a high-level API library, and it does not provide a direct connection to an X server, nor
 //! does it do anything else on its own. XRB's development purpose is to provide a foundation for
-//! higher-level Rust API wrapper libraries. It is used by [X.RS](https://crates.io/xrs), the
-//! official accompanying API library for XRB.
+//! higher-level Rust API wrapper libraries. It is used by [X.RS](https://crates.io/crates/xrs),
+//! the official accompanying API library for XRB.
 //!
 //! To demonstrate the difference between X Rust Bindings and a higher-level API library, here is a
-//! comparison between the same protocol message in XRB and its higher-level equivalent in X.RS:
+//! comparison between the same protocol message in XRB and its higher-level equivalent in
+//! [X.RS](https://crates.io/crates/xrs):
 //!
-//! ### [`ConnectionInit`](requests::ConnectionInit) request in XRB:
+//! ### `ConnectionInit` request in XRB
 //! ```rust
 //! /// A request to initiate a connection to the X server.
 //! pub struct ConnectionInit<'a> {
@@ -27,7 +28,7 @@
 //! }
 //! ```
 //!
-//! ### [`InitConnection`](xrs::req::InitConnection) request in X.RS:
+//! ### `InitConnection` request in X.RS
 //! ```rust
 //! /// A request to initiate a connection to the X server.
 //! pub struct InitConnection {}
