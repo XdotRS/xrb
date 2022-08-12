2f4729a126cb770b1168422c3ffe0c95e90f61c3
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-08-12 21:59:26 +0400

    XRB: predefine! & bitmask! macros

--- xrb/src/macros.rs
+++ xrb/src/macros.rs
@@ -2,75 +2,292 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-/// An internal-use macro for the main recursion of [`predefined_atoms`].
+/// Defines a series of sequential constants, starting at `1`.
 ///
-/// This is needed so that the first identifier can be treated differently and be set to 1.
-/// This is because every second element is set to the first plus 1; the first is never defined
-/// in [`predef_atoms_recurse`].
+/// # Example
+/// Consider the following syntax:
+/// ```rust
+/// predefine!(for Atom {
+///     STRUCTURE_NOTIFY,
+///     SUBSTRUCTURE_NOTIFY,
+///     /// If you are creating a window manager, you need to register for substructure
+///     /// redirection on the root window.
+///     SUBSTRUCTURE_REDIRECT,
+/// });
+/// ```
+/// This will generate the following code:
+/// ```rust
+/// #[allow(dead_code)]
+/// pub const STRUCTURE_NOTIFY: Atom = 1;
+/// #[allow(dead_code)]
+/// pub const SUBSTRUCTURE_NOTIFY: Atom = STRUCTURE_NOTIFY + 1;
+/// /// If you are creating a window manager, you need to register for substructure redirection on
+/// /// the root window.
+/// #[allow(dead_code)]
+/// pub const SUBSTRUCTURE_REDIRECT: Atom = SUBSTRUCTURE_NOTIFY + 1;
+/// ```
 #[macro_export(crate)]
-macro_rules! predef_atoms_recurse {
-	// Base case (only two atoms):
+macro_rules! predefine {
 	(
-        $a:ident, // capture `$a` (`$a` has already been assigned)
+		for $T:ty {
+			$(#[$a_atribs:meta])*
+			$a:ident$(,
+				$(#[$tail_atribs:meta])*
+				$tail:ident
+			)*
+		}
 
-        $(#[$b_atrib:meta])* // capture `$b`'s attributes (includes doc comments)
-        $b:ident // capture `$b`
-    ) => {
+		$($t:tt)*
+	) => {
+		$(#[$a_atribs])*
 		#[allow(dead_code)]
-        $(#[$b_atrib])* // apply `$b`'s attributes (includes doc comments)
-		pub const $b: crate::Atom = $a + 1; // define `$b` in terms of `$a`
+		pub const $a: $T = 1;
+
+		predefine! {
+			$T, $a$(,
+				$(#[$tail_atribs])*
+				$tail
+			)*
+		}
+
+		predefine! {
+			$($t)*
+		}
 	};
-	// Recursion (three or more atoms):
 	(
-        $a:ident, // capture `$a` (`$a` has already been assigned)
+		$T:ty, $previous:ident,
 
-        $(#[$b_atrib:meta])* // capture `$b`'s attributes (includes doc comments)
-        $b:ident, // capture `$b`
+		$(#[$a_atribs:meta])*
+		$a:ident$(,
+			$(#[$tail_atribs:meta])*
+			$tail:ident
+		)*
+	) => {
+		$(#[$a_atribs])*
+		#[allow(dead_code)]
+		pub const $a: $T = $previous + 1;
 
-        $( // capture one or more tail elements
-            $(#[$tail_atrib:meta])* // capture `$tail`s' attributes (includes doc comments)
-            $tail:ident // capture `$tail`s
-        ),+
-    ) => {
-        #[allow(dead_code)]
-        $(#[$b_atrib])* // apply `$b`'s attributes
-		pub const $b: crate::Atom = $a + 1; // define `$b` in terms of `$a`
-
-		crate::predef_atoms_recurse!( // recurse
-            $b,
-
-            $(
-                $(#[$tail_atrib])*
-                $tail
-            ),+
-        );
-	}
+		predefine! {
+			$T, $a$(,
+				$(#[$tail_atribs])*
+				$tail
+			)*
+		}
+	};
+	($T:ty, $a:ident) => {};
+	() => {};
 }
 
-/// Defines constants for [Atom](crate::Atom)s predefined in the X11 protocol, starting at index
-/// `1u8`.
+/// Generates a doc comment for the given tokens.
+///
+/// # Example
+/// Consider the following syntax:
+/// ```rust
+/// doc! {
+///     "This is a doc comment.",
+/// 	const TEXT: &str = "Hello, world!";
+/// }
+/// ```
+/// This will generate:
+/// ```rust
+/// /// This is a doc comment.
+/// const TEXT: &str = "Hello, world!";
+/// ```
 #[macro_export(crate)]
-macro_rules! predefined_atoms {
-    (
-        $(#[$a_atrib:meta])* // capture `$a`'s attributes (includes doc comments)
-        $a:ident, // capture `$a`
-
-        $( // recursively capture tail elements:
-            $(#[$tail_atrib:meta])* // their attributes (includes doc comments)
-            $tail:ident // tail elements themselves
-        ),+
-    ) => {
-        #[allow(dead_code)]
-        $(#[$a_atrib])* // apply `$a`'s attributes
-        pub const $a: crate::Atom = 1; // define `$a` as 1
-
-        crate::predef_atoms_recurse!( // recurse
-            $a,
-
-            $(
-                $(#[$tail_atrib])*
-                $tail
-            ),+
-        );
+macro_rules! doc {
+    ($x:expr, $($t:tt)+) => {
+        #[doc = $x]
+        $($t)+
     };
+}
+
+/// Creates a bitmask enum implementing [`Serialize`](crate::Serialize) and
+/// [`Deserialize`](crate::Deserialize).
+///
+/// # Example
+/// Consider the following syntax:
+/// ```rust
+/// bitmask! {
+///     /// A mask of keys and buttons.
+/// 	pub enum KeyButtonMask -> u16 {
+///         Shift => 0x0001,
+///         Lock => 0x0002,
+///         Control => 0x0004,
+///         Mod1 => 0x0008,
+///         Mod2 => 0x0010,
+///         Mod3 => 0x0020,
+///         /// Super key.
+///         Mod4 => 0x0040,
+///         Mod5 => 0x0080,
+///         /// Primary mouse button.
+///         Button1 => 0x0100,
+///         Button2 => 0x0200,
+///         Button3 => 0x0400,
+///         Button4 => 0x0800,
+///         Button5 => 0x1000,
+///     }
+/// }
+/// ```
+/// The following enum will be generated:
+/// ```rust
+/// /// A mask of keys and buttons.
+/// pub enum KeyButtonMask {
+///     Shift,
+///     Lock,
+///     Control,
+///     Mod1,
+///     Mod2,
+///     Mod3,
+///     /// Super key.
+///     Mod4,
+///     Mod5,
+///     /// Primary mouse button.
+///     Button1,
+///     Button2,
+///     Button3,
+///     Button4,
+///     Button5,
+/// }
+/// ```
+/// With the following implementation to get the masks:
+/// ```rust
+/// impl KeyButtonMask {
+///     /// Gets the bitmask associated with this `KeyButtonMask`.
+///     ///
+///     /// ```rust
+///     /// match self {
+///     ///     Self::Shift => 0x0001,
+///     ///     Self::Lock => 0x0002,
+///     ///     Self::Control => 0x0004,
+///     ///     Self::Mod1 => 0x0008,
+///     ///     Self::Mod2 => 0x0010,
+///     ///     Self::Mod3 => 0x0020,
+///     ///     Self::Mod4 => 0x0040,
+///     ///     Self::Mod5 => 0x0080,
+///     ///     Self::Button1 => 0x0100,
+///     ///     Self::Button2 => 0x0200,
+///     ///     Self::Button3 => 0x0400,
+///     ///     Self::Button4 => 0x0800,
+///     ///     Self::Button5 => 0x1000,
+///     /// }
+///     /// ```
+///     pub fn mask(&self) -> u16 {
+///         match self {
+///             Self::Shift => 0x0001,
+///             Self::Lock => 0x0002,
+///             Self::Control => 0x0004,
+///             Self::Mod1 => 0x0008,
+///             Self::Mod2 => 0x0010,
+///             Self::Mod3 => 0x0020,
+///             Self::Mod4 => 0x0040,
+///             Self::Mod5 => 0x0080,
+///             Self::Button1 => 0x0100,
+///             Self::Button2 => 0x0200,
+///             Self::Button3 => 0x0400,
+///             Self::Button4 => 0x0800,
+///             Self::Button5 => 0x1000,
+///         }
+///     }
+/// }
+/// ```
+/// And the following implementations for [`Serialize`](crate::Serialize) and
+/// [`Deserialize`](crate::Deserialize):
+/// ```rust
+/// impl crate::Serialize for KeyButtonMask {
+///     fn write(self, buf: &mut impl bytes::BufMut) {
+///         self.mask().write(buf);
+///     }
+/// }
+///
+/// impl crate::Deserialize for KeyButtonMask {
+///     fn read(buf: &mut impl bytes::Buf) -> Self {
+///         match u16::read(buf) {
+///             0x0001 => Self::Shift,
+///             0x0002 => Self::Lock,
+///             0x0004 => Self::Control,
+///             0x0008 => Self::Mod1,
+///             0x0010 => Self::Mod2,
+///             0x0020 => Self::Mod3,
+///             0x0040 => Self::Mod4,
+///             0x0080 => Self::Mod5,
+///             0x0100 => Self::Button1,
+///             0x0200 => Self::Button2,
+///             0x0400 => Self::Button3,
+///             0x0800 => Self::Button4,
+///             0x1000 => Self::Button5,
+///             _ => panic!(
+///                 "tried to read KeyButtonMask from Buf but no matching variant was found");
+///             ),
+///         }
+///     }
+/// }
+/// ```
+#[macro_export(crate)]
+macro_rules! bitmask {
+	(
+		$(#[$outer:meta])* // attributes/docs
+		$vis:vis enum $Mask:ident -> $T:ty { // pub enum Mask -> Type {
+			$(
+				$(#[$inner:meta])* // variant attributes/docs
+				$Variant:ident => $value:expr // Variant => value
+			),+$(,)*
+		}
+
+		$($t:tt)* // more bitmask definitions
+	) => {
+		$(#[$outer])* // attributes/docs
+		$vis enum $Mask { // pub enum Mask {
+			$(
+				$(#[$inner])* // variant attributes/docs
+				$Variant // Variant
+			),+
+		}
+
+		impl $Mask {
+			// Docs for `mask()` (messy, I know).
+			crate::doc!(concat!("Gets the bitmask associated with this `",
+				stringify!($Mask), "`.
+
+```rust
+match self {",
+				$(
+					concat!("
+    Self::", stringify!($Variant), " => ", stringify!($value), ","
+					)
+				),+, "
+}
+```"
+			),
+				pub fn mask(&self) -> $T { // pub fn mask(&self) -> Type {
+					match self {
+						$(Self::$Variant => $value),+ // Self::Variant => value,
+					}
+				}
+			);
+		}
+
+		impl crate::Serialize for $Mask {
+			fn write(self, buf: &mut impl bytes::BufMut) {
+				self.mask().write(buf);
+			}
+		}
+
+		impl crate::Deserialize for $Mask {
+			fn read(buf: &mut impl bytes::Buf) -> Self {
+				match <$T>::read(buf) {
+					$($value => Self::$Variant,)+ // value => Self::Variant,
+					_ => panic!(concat!(
+						"tried to read ",
+						stringify!($Mask),
+						" from Buf but no matching variant was found")
+					)
+				}
+			}
+		}
+
+		bitmask! {
+			$($t)* // repeat for any more bitmask definitions
+		}
+	};
+	() => {};
 }
