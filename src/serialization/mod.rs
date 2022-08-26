bae044dae0a3524fc1e943b5b326c95938289f99
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-08-26 10:14:29 +0400

    Added TODO: impl std::io:: Read + Write

--- src/serialization/mod.rs
+++ src/serialization/mod.rs
@@ -2,6 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+//! TODO: Implement [std::io::Read] and [std::io::Write] traits for serialization
+//! and deserialization.
+
 mod read_value;
 mod write_value;
 
