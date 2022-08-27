26d3b54c761d9835b5cbd647ddb11bbb4307dc3c
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-08-27 21:48:21 +0400

    Add documentation for some 'wrappers'

--- src/protocol/wrappers/mod.rs
+++ src/protocol/wrappers/mod.rs
@@ -8,8 +8,19 @@ mod wrapper_macro;
 use crate::protocol::common::values::{Timestamp, Window};
 
 wrappers! {
+	/// Allows fields to inherit their values from their 'parent'.
+	///
+	/// Similar in concept to an [`Option`] enum, this is a wrapper to provide
+	/// a [`CopyFromParent`](Inherit::CopyFromParent) alternate value for fields,
+	/// as defined in the X11 protocol.
 	pub enum Inherit<T> {
+		/// Wraps the indicated value, rather than inheriting the value.
 		Value(T),
+		/// Inherits the value from the 'parent'.
+		///
+		/// This does not _bind_ the value to that of its parent; that is,
+		/// changes to this value in the parent are not reflected. This merely
+		/// indicates that the corresponding value in the parent be _copied_.
 		CopyFromParent = 0,
 	}
 
@@ -18,11 +29,21 @@ wrappers! {
 		ParentRelative = 1,
 	}
 
+	/// Allows fields to indicate an 'any' state.
+	///
+	/// Similar in concept to an [`Option`] enum, this is a wrapper to provide
+	/// an [`Any`](Specificity::Any) alternate value for fields, as defined in
+	/// the X11 protocol.
 	pub enum Specificity<T> {
+		/// Wraps the indicated value, rather than representing an
+		/// [`Any`](Specificity::Any) state.
 		Specific(T),
+		/// Indicates an 'any' state; _that any input is allowed_, for example.
 		Any = 0,
 	}
 
+	/// Represents either a [`Specific`](Time::Specific) [`Timestamp`] or the
+	/// [`Current`] time.
 	pub enum Time {
 		Specific(Timestamp),
 		Current = 0,
