0705152ac7e2b1057dc66ef5f22e1b462ed6b66d
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-08-28 11:54:51 +0400

    script to auto-publish docs (if write access)

---
+++ publish-docs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Navigate to the directory this script is contained within, in case it is called from elsewhere.
+cd $(dirname $0)
+
+# Clone the docs branch into ./xrb-docs
+gh repo clone XdotRS/xrb ./xrb-docs -- --branch docs
+
+# Regenerate documentation.
+rm -r ./target/doc
+cargo doc --no-deps --document-private-items
+
+# Replace the `doc` folder with the newly generated docs
+rm -r ./xrb-docs/doc
+cp -r ./target/doc ./xrb-docs/
+
+# Commit and push the newly generated docs to the docos branch
+cd ./xrb-docs
+git add -A
+git commit --allow-empty -m "regenerated documentation"
+git push origin docs
+cd ..
+
+# Remove the cloned docs branch repo
+rm -rf ./xrb-docs
