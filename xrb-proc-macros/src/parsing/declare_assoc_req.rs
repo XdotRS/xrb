a9c9c407e4f281b670c31966ec3c57a8990d9fdf
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-08-30 17:20:30 +0400

    proc macros: request declaration

---
+++ xrb-proc-macros/src/parsing/declare_assoc_req.rs
@@ -0,0 +1,42 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use syn::parse::{Parse, ParseStream};
+use syn::{Result, Token, Type};
+
+/// A reply's declaration of the type of its associated request.
+///
+/// # Examples
+/// ```rust
+/// for GetWindowAttributes
+/// ```
+#[derive(Clone)]
+pub struct ReplyDeclaration {
+	pub request_ty: Type,
+}
+
+impl RequestDeclaration {
+	#[allow(dead_code)]
+	/// Construct a new [`RequestDeclaration`] with the given type of the
+	/// associated request.
+	pub fn new(request_ty: Type) -> Self {
+		Self { request_ty }
+	}
+}
+
+// Parsing {{{
+
+impl Parse for RequestDeclaration {
+	fn parse(input: ParseStream) -> Result<Self> {
+		// Parse the `for` token, but don't save it.
+		input.parse::<Token![for]>()?;
+
+		Ok(Self {
+			// Parse the request's type.
+			reply_ty: input.parse()?,
+		})
+	}
+}
+
+// }}}
--- xrb-proc-macros/src/parsing/mod.rs
+++ xrb-proc-macros/src/parsing/mod.rs
@@ -2,12 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+mod opcodes;
+
 mod definition;
 mod fields;
 mod metabyte;
+
 mod replies;
 mod requests;
+
 mod reply_title;
 mod request_title;
-mod opcodes;
+
+mod declare_assoc_req;
 mod declare_reply;
