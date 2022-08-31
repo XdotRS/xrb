0faf8c49d7dfa948567822db9b388301b36741d4
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-08-31 14:55:43 +0400

    Edit XRB/workspace metadata

--- publish-docs.sh
+++ publish-docs.sh
@@ -8,7 +8,7 @@ git clone --quiet --branch docs https://github.com/XdotRS/xrb ./xrb-docs
 
 # Regenerate documentation.
 rm -r ./target/doc
-cargo doc --no-deps
+cargo doc --no-deps --workspace --document-private-items
 
 # Replace the `doc` folder with the newly generated docs
 rm -r ./xrb-docs/doc
