28456c5fb7f10326226db43c22e327513ad29419
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-09-05 10:56:18 +0400

    cornflakes: crate-level docs

--- cornflakes/src/lib.rs
+++ cornflakes/src/lib.rs
@@ -2,6 +2,19 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+//! # Cornflakes
+//!
+//! Cornflakes is a utility library developed for [X Rust Bindings]. It provides
+//! traits for extending the functionality of [bytes]' [`Buf`] and [`BufMut`]
+//! traits with some utilities relevant to XRB, as well as [`ToBytes`] and
+//! [`FromBytes`] traits for (de)serialization, and [`ByteSize`] and
+//! [`StaticByteSize`] traits for getting the byte size a type is written as.
+//!
+//! [X Rust Bindings]: https://docs.aquariwm.org/doc/xrb/
+//! [bytes]: https://docs.rs/bytes/latest/bytes/
+//! [`Buf`]: https://docs.rs/bytes/latest/bytes/trait.Buf.html
+//! [`BufMut`]: https://docs.rs/bytes/latest/bytes/trait.BufMut.html
+
 mod implementations;
 mod traits;
 
