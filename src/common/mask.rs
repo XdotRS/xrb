0e894589c28cc70f1a8a58aa2e463f20c72dd83b
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-01-01 22:11:17 +0000

    derive DataSize, Readable, Writable for masks

--- src/common/mask.rs
+++ src/common/mask.rs
@@ -3,9 +3,10 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use bitflags::bitflags;
+use xrbk_macro::{DataSize, Readable, Writable};
 
 bitflags! {
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct ColorChannelMask: u8 {
 		/// Whether the red color channel is enabled.
 		const DO_RED = 0x01;
@@ -16,7 +17,7 @@ bitflags! {
 	}
 
 	/// A mask of events.
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct EventMask: u32 {
 		/// Key press events.
 		const KEY_PRESS = 0x0000_0001;
@@ -143,7 +144,7 @@ bitflags! {
 	/// - `PROPERTY_CHANGE`
 	/// - `COLORMAP_CHANGE`
 	/// - `OWNER_GRAB_BUTTON`
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct PointerEventMask: u32 {
 		// removes KEY_PRESS and KEY_RELEASE
 		/// Mouse button press events.
@@ -217,7 +218,7 @@ bitflags! {
 	/// - `PROPERTY_CHANGE`
 	/// - `COLORMAP_CHANGE`
 	/// - `OWNER_GRAB_BUTTON`
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct DeviceEventMask: u32 {
 		/// Key press events.
 		const KEY_PRESS = 0x0000_0001;
@@ -258,7 +259,7 @@ bitflags! {
 	///
 	/// This is the same as [`ModifierKeyMask`], but with masks for currently
 	/// held mouse buttons.
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct ModifierMask: u16 {
 		/// Whether `Shift` is held.
 		const SHIFT = 0x0001;
@@ -306,7 +307,7 @@ bitflags! {
 	/// mask for [`ANY_MODIFIER`].
 	///
 	/// [`ANY_MODIFIER`]: AnyModifierKeyMask::ANY_MODIFIER
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct ModifierKeyMask: u16 {
 		/// Whether `Shift` is held.
 		const SHIFT = 0x0001;
@@ -336,7 +337,7 @@ bitflags! {
 	/// [`ANY_MODIFIER`].
 	///
 	/// [`ANY_MODIFIER`]: AnyModifierKeyMask::ANY_MODIFIER
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct AnyModifierKeyMask: u16 {
 		/// Whether `Shift` is held.
 		const SHIFT = 0x0001;
@@ -431,7 +432,7 @@ bitflags! {
 	/// [Cursor]: crate::x11::Attribute::Cursor
 	/// [`EventMask::none()`]: EventMask::none
 	/// [`DeviceEventMask::none()`]: DeviceEventMask::none
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct AttributeMask: u32 {
 		/// See also: [`BackgroundPixmap`]
 		///
@@ -495,7 +496,7 @@ bitflags! {
 		const CURSOR = 0x0000_4000;
 	}
 
-	#[derive(Default)]
+	#[derive(Default, DataSize, Readable, Writable)]
 	pub struct ConfigureWindowMask: u16 {
 		const X = 0x0001;
 		const Y = 0x0002;
