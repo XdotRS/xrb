0b9a5346a383350a3e967a83ae2de4d826a9dbcc
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-01-02 15:55:22 +0000

    macro_rules! macro to predefine atoms

--- src/common/atom.rs
+++ src/common/atom.rs
@@ -36,71 +36,87 @@ derive_xrb! {
 	}
 }
 
-pub const PRIMARY: Atom = Atom::new(1);
-pub const SECONDARY: Atom = Atom::new(2);
-pub const ARC: Atom = Atom::new(3);
-pub const ATOM: Atom = Atom::new(4);
-pub const BITMAP: Atom = Atom::new(5);
-pub const CARDINAL: Atom = Atom::new(6);
-pub const COLORMAP: Atom = Atom::new(7);
-pub const CURSOR: Atom = Atom::new(8);
-pub const CUT_BUFFER0: Atom = Atom::new(9);
-pub const CUT_BUFFER1: Atom = Atom::new(10);
-pub const CUT_BUFFER2: Atom = Atom::new(11);
-pub const CUT_BUFFER3: Atom = Atom::new(12);
-pub const CUT_BUFFER4: Atom = Atom::new(13);
-pub const CUT_BUFFER5: Atom = Atom::new(14);
-pub const CUT_BUFFER6: Atom = Atom::new(15);
-pub const CUT_BUFFER7: Atom = Atom::new(16);
-pub const DRAWABLE: Atom = Atom::new(17);
-pub const FONT: Atom = Atom::new(18);
-pub const INTEGER: Atom = Atom::new(19);
-pub const PIXMAP: Atom = Atom::new(20);
-pub const POINT: Atom = Atom::new(21);
-pub const RECTANGLE: Atom = Atom::new(22);
-pub const RESOURCE_MANAGER: Atom = Atom::new(23);
-pub const RGB_COLOR_MAP: Atom = Atom::new(24);
-pub const RGB_BEST_MAP: Atom = Atom::new(25);
-pub const RGB_BLUE_MAP: Atom = Atom::new(26);
-pub const RGB_DEFAULT_MAP: Atom = Atom::new(27);
-pub const RGB_GRAY_MAP: Atom = Atom::new(28);
-pub const RGB_GREEN_MAP: Atom = Atom::new(29);
-pub const RGB_RED_MAP: Atom = Atom::new(30);
-pub const STRING: Atom = Atom::new(31);
-pub const VISUALID: Atom = Atom::new(32);
-pub const WINDOW: Atom = Atom::new(33);
-pub const WM_COMMAND: Atom = Atom::new(34);
-pub const WM_HINTS: Atom = Atom::new(35);
-pub const WM_CLIENT_MACHINE: Atom = Atom::new(36);
-pub const WM_ICON_NAME: Atom = Atom::new(37);
-pub const WM_ICON_SIZE: Atom = Atom::new(38);
-pub const WM_NAME: Atom = Atom::new(39);
-pub const WM_NORMAL_HINTS: Atom = Atom::new(40);
-pub const WM_SIZE_HINTS: Atom = Atom::new(41);
-pub const WM_ZOOM_HINTS: Atom = Atom::new(42);
-pub const MIN_SPACE: Atom = Atom::new(43);
-pub const NORM_SPACE: Atom = Atom::new(44);
-pub const MAX_SPACE: Atom = Atom::new(45);
-pub const END_SPACE: Atom = Atom::new(46);
-pub const SUPERSCRIPT_X: Atom = Atom::new(47);
-pub const SUPERSCRIPT_Y: Atom = Atom::new(48);
-pub const SUBSCRIPT_X: Atom = Atom::new(49);
-pub const SUBSCRIPT_Y: Atom = Atom::new(50);
-pub const UNDERLINE_POSITION: Atom = Atom::new(51);
-pub const UNDERLINE_THICKNESS: Atom = Atom::new(52);
-pub const STRIKEOUT_ASCENT: Atom = Atom::new(53);
-pub const STRIKEOUT_DESCENT: Atom = Atom::new(54);
-pub const ITALIC_ANGLE: Atom = Atom::new(55);
-pub const X_HEIGHT: Atom = Atom::new(56);
-pub const QUAD_WIDTH: Atom = Atom::new(57);
-pub const WEIGHT: Atom = Atom::new(58);
-pub const POINT_SIZE: Atom = Atom::new(59);
-pub const RESOLUTION: Atom = Atom::new(60);
-pub const COPYRIGHT: Atom = Atom::new(61);
-pub const NOTICE: Atom = Atom::new(62);
-pub const FONT_NAME: Atom = Atom::new(63);
-pub const FAMILY_NAME: Atom = Atom::new(64);
-pub const FULL_NAME: Atom = Atom::new(65);
-pub const CAP_HEIGHT: Atom = Atom::new(66);
-pub const WM_CLASS: Atom = Atom::new(67);
-pub const WM_TRANSIENT_FOR: Atom = Atom::new(68);
+macro_rules! atoms {
+	(
+		$(
+			$(#[$attr:meta])*
+			$ATOM:ident = $id:expr
+		),*$(,)?
+	) => {
+		$(
+			$(#[$attr])*
+			pub const $ATOM: Atom = Atom::new($id);
+		)*
+	}
+}
+
+atoms! {
+	PRIMARY = 1,
+	SECONDARY = 2,
+	ARC = 3,
+	ATOM = 4,
+	BITMAP = 5,
+	CARDINAL = 6,
+	COLORMAP = 7,
+	CURSOR = 8,
+	CUT_BUFFER0 = 9,
+	CUT_BUFFER1 = 10,
+	CUT_BUFFER2 = 11,
+	CUT_BUFFER3 = 12,
+	CUT_BUFFER4 = 13,
+	CUT_BUFFER5 = 14,
+	CUT_BUFFER6 = 15,
+	CUT_BUFFER7 = 16,
+	DRAWABLE = 17,
+	FONT = 18,
+	INTEGER = 19,
+	PIXMAP = 20,
+	POINT = 21,
+	RECTANGLE = 22,
+	RESOURCE_MANAGER = 23,
+	RGB_COLOR_MAP = 24,
+	RGB_BEST_MAP = 25,
+	RGB_BLUE_MAP = 26,
+	RGB_DEFAULT_MAP = 27,
+	RGB_GRAY_MAP = 28,
+	RGB_GREEN_MAP = 29,
+	RGB_RED_MAP = 30,
+	STRING = 31,
+	VISUALID = 32,
+	WINDOW = 33,
+	WM_COMMAND = 34,
+	WM_HINTS = 35,
+	WM_CLIENT_MACHINE = 36,
+	WM_ICON_NAME = 37,
+	WM_ICON_SIZE = 38,
+	WM_NAME = 39,
+	WM_NORMAL_HINTS = 40,
+	WM_SIZE_HINTS = 41,
+	WM_ZOOM_HINTS = 42,
+	MIN_SPACE = 43,
+	NORM_SPACE = 44,
+	MAX_SPACE = 45,
+	END_SPACE = 46,
+	SUPERSCRIPT_X = 47,
+	SUPERSCRIPT_Y = 48,
+	SUBSCRIPT_X = 49,
+	SUBSCRIPT_Y = 50,
+	UNDERLINE_POSITION = 51,
+	UNDERLINE_THICKNESS = 52,
+	STRIKEOUT_ASCENT = 53,
+	STRIKEOUT_DESCENT = 54,
+	ITALIC_ANGLE = 55,
+	X_HEIGHT = 56,
+	QUAD_WIDTH = 57,
+	WEIGHT = 58,
+	POINT_SIZE = 59,
+	RESOLUTION = 60,
+	COPYRIGHT = 61,
+	NOTICE = 62,
+	FONT_NAME = 63,
+	FAMILY_NAME = 64,
+	FULL_NAME = 65,
+	CAP_HEIGHT = 66,
+	WM_CLASS = 67,
+	WM_TRANSIENT_FOR = 68,
+}
