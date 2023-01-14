771f9b0cc8482557eea854ebb6c8217ca132afa1
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-01-14 15:51:41 +0000

    XRBK: improve Wrapper trait

--- src/common.rs
+++ src/common.rs
@@ -378,9 +378,9 @@ derive_xrb! {
 	}
 
 	impl Wrapper for WindowClass {
-		type WrappedType = u16;
+		type Wrapped = u16;
 
-		fn wrap(val: Self::WrappedType) -> Self {
+		fn wrap(val: Self::Wrapped) -> Self {
 			match val {
 				discrim if discrim == 1 => Self::InputOutput,
 				discrim if discrim == 2 => Self::InputOnly,
@@ -391,7 +391,7 @@ derive_xrb! {
 			}
 		}
 
-		fn unwrap(&self) -> &Self::WrappedType {
+		fn unwrap(&self) -> &Self::Wrapped {
 			match self {
 				Self::InputOutput => &1,
 				Self::InputOnly => &2,
