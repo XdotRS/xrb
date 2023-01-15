4865de4d1053bf94a6154eceb2cff21177a377cb
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-01-15 21:37:18 +0000

    docs for x11::error::Request

--- src/x11/error.rs
+++ src/x11/error.rs
@@ -9,6 +9,13 @@ extern crate self as xrb;
 
 derive_xrb! {
 	#[derive(Debug, Hash, Writable, Readable, X11Size)]
+	/// An [error] generated when the [major opcode] and [minor opcode]
+	/// combination provided in a [request] does not specify a valid [request].
+	///
+	/// [error]: Error
+	/// [request]: crate::message::Request
+	/// [major opcode]: crate::message::Request::MAJOR_OPCODE
+	/// [minor opcode]: crate::message::Request::MINOR_OPCODE
 	pub struct Request: Error(1) {
 		#[sequence]
 		/// The [sequence number][sequence] identifying the [request] that was
@@ -21,8 +28,29 @@ derive_xrb! {
 		pub sequence: u16,
 
 		#[minor_opcode]
+		/// The [minor opcode] meant to refer to the type of [request] that was
+		/// sent.
+		///
+		/// In this case, the combination of this [minor opcode] and the
+		/// `major_opcode` did not actually refer to a valid [request].
+		///
+		/// See [`Request::MINOR_OPCODE`][minor opcode] for more information.
+		///
+		/// [request]: crate::message::Request
+		/// [minor opcode]: crate::message::Request::MINOR_OPCODE
 		pub minor_opcode: u16,
+
 		#[major_opcode]
+		/// The [major opcode] meant to refer to the type of [request] that was
+		/// sent.
+		///
+		/// In this case, the combination of this [major opcode] and the
+		/// `minor_opcode` did not actually refer to a valid [request].
+		///
+		/// See [`Request::MAJOR_OPCODE`][major opcode] for more information.
+		///
+		/// [request]: crate::message::Request
+		/// [major opcode]: crate::message::Request::MAJOR_OPCODE
 		pub major_opcode: u8,
 		[_; ..],
 	}
