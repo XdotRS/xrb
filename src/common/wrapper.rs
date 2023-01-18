b4afbc990398e1cfedc7960b31dfe083444e0e66
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-01-18 08:17:30 +0000

    manually implement wrapper enums

--- src/common/wrapper.rs
+++ src/common/wrapper.rs
@@ -2,78 +2,319 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::{Atom, Colormap, Timestamp, Window};
-use bytes::{Buf, BufMut};
-use xrbk::{ConstantX11Size, ReadResult, Readable, Wrapper, Writable, WriteResult, X11Size};
+use xrbk::{Buf, BufMut, ConstantX11Size, X11Size, Readable, Writable, ReadResult, WriteResult, ReadError};
 
-macro_rules! impl_wrapper {
-	(
-		$($Type:ty: $wrapped:ty),*$(,)?
-	) => {
-		$(
-			impl Wrapper for $Type {
-				type WrappedType = $wrapped;
+use crate::{Atom, Button, Colormap, Keycode, Pixmap, Timestamp, Window, WindowClass};
 
-				fn wrap(val: $wrapped) -> Self {
-					Self(val)
-				}
-
-				fn unwrap(&self) -> &$wrapped {
-					&self.0
-				}
-			}
-		)*
-	}
-}
-
-impl_wrapper! {
-	Window: u32,
-	Colormap: u32,
-
-	Atom: u32,
-}
-
-#[derive(Debug, Hash)]
-pub enum Inheritable<T> {
-	CopyFromParent,
-	Uninherited(T),
-}
-
-#[derive(Debug, Hash)]
-pub enum CurrentableTime {
-	CurrentTime,
-	Timestamp(Timestamp),
-}
-
-impl X11Size for CurrentableTime {
-	fn x11_size(&self) -> usize {
-		Self::X11_SIZE
-	}
-}
-
-impl ConstantX11Size for CurrentableTime {
-	const X11_SIZE: usize = 4;
-}
-
-impl Writable for CurrentableTime {
-	fn write_to(&self, buf: &mut impl BufMut) -> WriteResult {
-		match self {
-			Self::CurrentTime => buf.put_u32(0),
-			Self::Timestamp(timestamp) => timestamp.write_to(buf)?,
+macro_rules! impl_constant_x11_size {
+	($type:ty {
+		$($token:tt)*
+	}) => {
+		impl ConstantX11Size for $type {
+			const X11_SIZE: usize = { $($token)* };
 		}
 
-		Ok(())
+		impl X11Size for $type {
+			fn x11_size(&self) -> usize {
+				Self::X11_SIZE
+			}
+		}
 	}
 }
 
-impl Readable for CurrentableTime {
-	fn read_from(buf: &mut impl Buf) -> ReadResult<Self>
-	where
-		Self: Sized,
-	{
-		Ok(match buf.get_u32() {
-			x if x == 0 => Self::CurrentTime,
-			timestamp => Self::Timestamp(Timestamp::new(timestamp)),
-		})
+macro_rules! impl_readable {
+	($type:ty: $buf:ident {
+		$($token:tt)*
+	}) => {
+		impl Readable for $type {
+			fn read_from($buf: &mut impl Buf) -> ReadResult<Self> {
+				$($token)*
+			}
+		}
+	};
+}
+
+macro_rules! impl_writable {
+	($type:ty: &$self:ident, $buf:ident {
+		$($token:tt)*
+	}) => {
+		impl Writable for $type {
+			fn write_to(&$self, $buf: &mut impl BufMut) -> WriteResult {
+				$($token)*
+			}
+		}
 	}
 }
+
+pub enum CopyableFromParent<T> { // {{{
+	CopyFromParent,
+	Other(T),
+}
+
+impl_constant_x11_size!(CopyableFromParent<WindowClass> {
+	WindowClass::X11_SIZE
+});
+
+impl_readable!(CopyableFromParent<WindowClass>: buf {
+	match buf.get_u32() {
+		discrim if discrim == 0 => Ok(Self::CopyFromParent),
+
+		discrim if discrim == 1 => Ok(Self::Other(WindowClass::InputOutput)),
+		discrim if discrim == 2 => Ok(Self::Other(WindowClass::InputOnly)),
+
+		other_discrim => Err(ReadError::UnrecognizedDiscriminant(other_discrim as usize)),
+	}
+});
+
+impl_writable!(CopyableFromParent<WindowClass>: &self, buf {
+	match self {
+		Self::CopyFromParent => buf.put_u32(0),
+		Self::Other(class) => class.write_to(buf)?,
+	}
+
+	Ok(())
+});
+
+impl_constant_x11_size!(CopyableFromParent<Pixmap> {
+	Pixmap::X11_SIZE
+});
+
+impl_readable!(CopyableFromParent<Pixmap>: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::CopyFromParent,
+		val => Self::Other(Pixmap::new(val)),
+	})
+});
+
+impl_writable!(CopyableFromParent<Pixmap>: &self, buf {
+	match self {
+		Self::CopyFromParent => buf.put_u32(0),
+		Self::Other(val) => val.write_to(buf)?,
+	}
+
+	Ok(())
+});
+
+impl_constant_x11_size!(CopyableFromParent<Colormap> {
+	Colormap::X11_SIZE
+});
+
+impl_readable!(CopyableFromParent<Colormap>: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::CopyFromParent,
+		val => Self::Other(Colormap::new(val)),
+	})
+});
+
+impl_writable!(CopyableFromParent<Colormap>: &self, buf {
+	match self {
+		Self::CopyFromParent => buf.put_u32(0),
+		Self::Other(val) => val.write_to(buf)?,
+	}
+
+	Ok(())
+}); // }}}
+
+pub enum ParentRelatable<T> { // {{{
+	ParentRelative,
+	Other(T),
+}
+
+impl_constant_x11_size!(ParentRelatable<Option<Pixmap>> {
+	Pixmap::X11_SIZE
+});
+
+impl_readable!(ParentRelatable<Option<Pixmap>>: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::Other(None),
+
+		discrim if discrim == 1 => Self::ParentRelative,
+
+		val => Self::Other(Some(Pixmap::new(val))),
+	})
+});
+
+impl_writable!(ParentRelatable<Option<Pixmap>>: &self, buf {
+	match self {
+		Self::ParentRelative => buf.put_u32(1),
+
+		Self::Other(None) => buf.put_u32(0),
+		Self::Other(Some(pixmap)) => pixmap.write_to(buf)?,
+	}
+
+	Ok(())
+}); // }}}
+
+pub enum Any<T> { // {{{
+	Any,
+	Other(T),
+}
+
+impl_constant_x11_size!(Any<Atom> {
+	Atom::X11_SIZE
+});
+
+impl_readable!(Any<Atom>: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::Any,
+		val => Self::Other(Atom::new(val)),
+	})
+});
+
+impl_writable!(Any<Atom>: &self, buf {
+	match self {
+		Self::Any => buf.put_u32(0),
+		Self::Other(atom) => atom.write_to(buf)?,
+	}
+
+	Ok(())
+});
+
+impl_constant_x11_size!(Any<Button> {
+	Button::X11_SIZE
+});
+
+impl_readable!(Any<Button>: buf {
+	Ok(match buf.get_u8() {
+		discrim if discrim == 0 => Self::Any,
+		val => Self::Other(Button::new(val)),
+	})
+});
+
+impl_writable!(Any<Button>: &self, buf {
+	match self {
+		Self::Any => buf.put_u8(0),
+		Self::Other(button) => button.write_to(buf)?,
+	}
+
+	Ok(())
+});
+
+impl_constant_x11_size!(Any<Keycode> {
+	Keycode::X11_SIZE
+});
+
+impl_readable!(Any<Keycode>: buf {
+	Ok(match buf.get_u8() {
+		discrim if discrim == 0 => Self::Any,
+		val => Self::Other(Keycode::new(val)),
+	})
+});
+
+impl_writable!(Any<Keycode>: &self, buf {
+	match self {
+		Self::Any => buf.put_u8(0),
+		Self::Other(keycode) => keycode.write_to(buf)?,
+	}
+
+	Ok(())
+}); // }}}
+
+pub enum CurrentableTime { // {{{
+	CurrentTime,
+	Other(Timestamp),
+}
+
+impl_constant_x11_size!(CurrentableTime {
+	Timestamp::X11_SIZE
+});
+
+impl_readable!(CurrentableTime: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::CurrentTime,
+		val => Self::Other(Timestamp::new(val)),
+	})
+});
+
+impl_writable!(CurrentableTime: &self, buf {
+	match self {
+		Self::CurrentTime => buf.put_u32(0),
+		Self::Other(timestamp) => timestamp.write_to(buf)?,
+	}
+
+	Ok(())
+}); // }}}
+
+pub enum DestinationWindow { // {{{
+	Cursor,
+	Focus,
+	Other(Window),
+}
+
+impl_constant_x11_size!(DestinationWindow {
+	Window::X11_SIZE
+});
+
+impl_readable!(DestinationWindow: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::Cursor,
+		discrim if discrim == 1 => Self::Focus,
+
+		val => Self::Other(Window::new(val)),
+	})
+});
+
+impl_writable!(DestinationWindow: &self, buf {
+	match self {
+		Self::Cursor => buf.put_u32(0),
+		Self::Focus => buf.put_u32(1),
+
+		Self::Other(window) => window.write_to(buf)?,
+	}
+
+	Ok(())
+}); // }}}
+
+pub enum FocusWindow { // {{{
+	None,
+	CursorRoot,
+	Other(Window),
+}
+
+impl_constant_x11_size!(FocusWindow {
+	Window::X11_SIZE
+});
+
+impl_readable!(FocusWindow: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::None,
+		discrim if discrim == 1 => Self::CursorRoot,
+
+		val => Self::Other(Window::new(val)),
+	})
+});
+
+impl_writable!(FocusWindow: &self, buf {
+	match self {
+		Self::None => buf.put_u32(0),
+		Self::CursorRoot => buf.put_u32(1),
+	}
+
+	Ok(())
+}); // }}}
+
+pub enum KillClientTarget { // {{{
+	AllTemporarilyRetainedClients,
+	Other(u32),
+}
+
+impl_constant_x11_size!(KillClientTarget {
+	u32::X11_SIZE
+});
+
+impl_readable!(KillClientTarget: buf {
+	Ok(match buf.get_u32() {
+		discrim if discrim == 0 => Self::AllTemporarilyRetainedClients,
+
+		val => Self::Other(val),
+	})
+});
+
+impl_writable!(KillClientTarget: &self, buf {
+	match self {
+		Self::AllTemporarilyRetainedClients => buf.put_u32(0),
+		Self::Other(val) => buf.put_u32(*val),
+	}
+
+	Ok(())
+}); // }}}
